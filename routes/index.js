6a09149dfa6bf82ec445181bfe03377b97a92f8f
Hunterpp <hunter.p@h.p>
2021-02-13 23:57:21 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,12 @@ var KalturaClientFactory = require('../lib/kalturaClientFactory');
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   try {
-    //you probably want to make an api call with client
+    //you probably want to make an api call with client, see https://developer.kaltura.com/
     var adminks = await KalturaClientFactory.getKS('', { type: kaltura.enums.SessionType.ADMIN });
     var client = await KalturaClientFactory.getClient(adminks);
 
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Kaltura Node Template' });
+
   } catch (e) {
     res.render('error', { message: e, error: e });
   }
